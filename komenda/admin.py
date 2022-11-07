af4681079bb6d7bcf423182f919888b64771e440
akacper <kacper.adamski@student.uwm.edu.pl>
2022-11-07 11:44:52 +0100

    register models, fix gitignore

--- komenda/admin.py
+++ komenda/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from komenda.models import *
 
-# Register your models here.
+
+admin.site.register(Obywatel)
+admin.site.register(Pracownik)
+admin.site.register(Oddzial)
+admin.site.register(Sprawa)
+admin.site.register(StronyWSprawie)
+admin.site.register(Samochod)
+admin.site.register(Szkoda)
